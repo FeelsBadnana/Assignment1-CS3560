a673e346e2a3f90c2bcbe71f4dde04b1f9e7ce14
theJ4 <56046930+theJ4@users.noreply.github.com>
2021-03-08 09:27:05 -0800

    Update BlackHole.java

--- src/others/BlackHole.java
+++ src/others/BlackHole.java
@@ -1,22 +1,24 @@
 package src.others;
 
+//Black Holes can occur when a Star reaches the end of its life cycle. Their large gravitational pull makes it so it can 'absorb' many other Celestial Bodies to add to their mass
 public class BlackHole {
     private float mass;
     private float angularMomentum;
     private float schwarzchildRadius;
     
-    public void setMass(){
+    public void setMass(float m){
     }
     public float getMass(){
     }
-    public void setMomentum(){
+    public void setMomentum(float momentum){
     }
     public float getMomentum(){
     }
-    public void setRadius(){
+    public void setRadius(float radius){
     }
     public float getRadius(){
     }
+    //remove body from its respective system and add its mass to this.mass
     public void absorb(CelestialBody body){
     }
 }
