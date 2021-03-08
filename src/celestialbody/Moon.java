f20c5f82bdf1cfc1eacbdba61254eee364839e8f
Sleepyawk <78984417+Sleepyawk@users.noreply.github.com>
2021-03-08 09:42:33 -0800

    Update Moon.java
    Added comments

--- src/celestialbody/Moon.java
+++ src/celestialbody/Moon.java
@@ -1,17 +1,23 @@
 package src.celestialbody;
 
+//Sets up the qualities of a moon
 public class Moon {
-	private float orbitalSpeed;
-	private float orbitalPeriod;
+	private float orbitalSpeed; //the orbital speed of the moon
+	private float orbitalPeriod; // the orbital period of the moon
+		
+		//changes speed of orbitalSpeed
 		public void setOrbitalSpeed(float OSpeed){
 			orbitalSpeed = OSpeed;
 		}
+		//changes period of orbitalPeriod
 		public void setOrbitalPeriod(float OPeriod){
 		orbitalPeriod = OPeriod;
 		}
+		//returns value of orbitalSpeed
 		public float getOSpeed(){
 			return orbitalSpeed;
 		}
+		//returns value of orbitalPeriod
 		public float getOPeriod(){
 			return orbitalPeriod;
 		}
