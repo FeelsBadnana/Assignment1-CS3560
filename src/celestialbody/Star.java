2268f5e7b64e1f0f9dc3fcfaf9465b649541a318
theJ4 <56046930+theJ4@users.noreply.github.com>
2021-03-08 09:22:13 -0800

    Update Star.java

--- src/celestialbody/Star.java
+++ src/celestialbody/Star.java
@@ -1,29 +1,32 @@
 package src.celestialbody;
 
 public class Star {
+    //starType determines the stage of life that the star is in
     private String starType;
     private float mass;
     private float surfaceTemperature;
     private float stellarLuminosity;
     
-    public void setType(){
+    public void setType(String type){
     }
     public String getType(){ 
     }
-    public void setMass(){
+    public void setMass(float m){
     }
     public float getMass(){
     }
-    public void setTemp(){
+    public void setTemp(float temp){
     }
     public float getTemp(){
     }
-    public void setLumin(){
+    public void setLumin(float lumin){
     }
     public float getLumin(){
     }
+    //Calculate the color of the star by using this.surfaceTemperature
     public String getColor(){
     }
+    //Returns a BlackHole that this star could transform into at the end of its life cycle
     public BlackHole transform(){
     }
 }
