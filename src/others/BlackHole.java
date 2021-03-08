2ab98190736ffaf9f3a6262f4575e61e7f2a9f26
theJ4 <56046930+theJ4@users.noreply.github.com>
2021-03-07 16:26:45 -0800

    Update BlackHole.java

--- src/others/BlackHole.java
+++ src/others/BlackHole.java
@@ -1,5 +1,22 @@
 package src.others;
 
 public class BlackHole {
+    private float mass;
+    private float angularMomentum;
+    private float schwarzchildRadius;
     
+    public void setMass(){
+    }
+    public float getMass(){
+    }
+    public void setMomentum(){
+    }
+    public float getMomentum(){
+    }
+    public void setRadius(){
+    }
+    public float getRadius(){
+    }
+    public void absorb(CelestialBody body){
+    }
 }
