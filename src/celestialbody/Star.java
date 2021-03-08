3c7276ac05bf34e2ca86fb62b7ac9ff250e93605
theJ4 <56046930+theJ4@users.noreply.github.com>
2021-03-07 16:23:38 -0800

    Update Star.java

--- src/celestialbody/Star.java
+++ src/celestialbody/Star.java
@@ -1,5 +1,29 @@
 package src.celestialbody;
 
 public class Star {
+    private String starType;
+    private float mass;
+    private float surfaceTemperature;
+    private float stellarLuminosity;
     
+    public void setType(){
+    }
+    public String getType(){ 
+    }
+    public void setMass(){
+    }
+    public float getMass(){
+    }
+    public void setTemp(){
+    }
+    public float getTemp(){
+    }
+    public void setLumin(){
+    }
+    public float getLumin(){
+    }
+    public String getColor(){
+    }
+    public BlackHole transform(){
+    }
 }
